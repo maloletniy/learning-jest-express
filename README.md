24a977dfb053583f62624ee4468a092f01a18c1f
Igor Maloletnii <igor.maloletnii@kuehne-nagel.com>
2020-02-25 17:44:58 +0200

    first test to pass

---
+++ README.md
@@ -0,0 +1,8 @@
+##Dependencies:
+
+- eslint for code formatting
+- babel-node to support ES6 for server side node
+- nodemon to reload the code on the fly
+
+##How to run:
+`npm run serve` to start the server on port 3000
---
+++ tests/todo.controller.test.js
@@ -0,0 +1,7 @@
+import TodoController from "../controllers/todo.controller"
+
+describe("TodoController.createTodo", () => {
+    it("should have a createTodo function", () => {
+        expect(typeof TodoController.createTodo).toBe("function")
+    })
+})
